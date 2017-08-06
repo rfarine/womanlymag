4bcf28522d2bb9c680a49c08c2eeb79018515421
Rae Farine <rfarine1027@gmail.com>
2017-08-06 11:33:43 -0400

    larger text size if only 1 content item and short amount of text

--- src/templates/article/article.jsx
+++ src/templates/article/article.jsx
@@ -1,111 +1,154 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import _ from 'lodash';
 import Remarkable from 'remarkable';
 import RemarkableReactRenderer from 'remarkable-react';
 import ReactPlayer from 'react-player';
 import ReactAudioPlayer from 'react-audio-player';
+import cn from 'classnames';
 import config from '../../../gatsby-config.js';
 import style from './article.module.scss';
 
 const md = new Remarkable();
 md.renderer = new RemarkableReactRenderer();
 
-function getTags(tags) {
-  const seoTags = _.flatMap(tags, (tag) => {
-    return tag.seoMetaTags;
-  });
+export class Article extends Component {
+  state = {
+    hasOneContentItem: 0,
+    textLength: 0,
+  }
 
-  const titleTags = _.map(
-    _.filter(seoTags, { tagName: 'title' }),
-    'content'
-  );
+  componentWillMount() {
+    const article = this.props.data.markdownRemark.frontmatter;
 
-  return `${titleTags.join(', ')}`;
-};
+    this.hasOneContentItem();
 
-function renderContent(data) {
-  let content = [];
+    if (article.text) {
+      this.setState({
+        textLength: article.text.length,
+      });
+    }
+  }
 
-  if (data.images) {
-    _.map(data.images, (image) => {
-      content.push(
-        <img
-          className={style.image}
-          src={image.image.url}
-          alt={image.image.title}
-        />
-      );
+  hasOneContentItem = () => {
+    const article = this.props.data.markdownRemark.frontmatter;
+    const hasOneImage = article.images && article.images.length === 1 && !article.audio && !article.video;
+    const hasOnlyAudio = !article.images && article.audio && !article.video;
+    const hasOnlyVideo = !article.images && !article.audio && article.video;
+
+    this.setState({
+      hasOneContentItem:
+        hasOneImage ||
+        hasOnlyAudio ||
+        hasOnlyVideo,
     });
   }
 
-  if (data.video) {
-    content.push(
-      <ReactPlayer
-        className={style.video}
-        url={data.video.url}
-        width="100%"
-        height="auto"
-      />
+  getTags = () => {
+    const article = this.props.data.markdownRemark.frontmatter;
+
+    const seoTags = _.flatMap(article.tags, (tag) => {
+      return tag.seoMetaTags;
+    });
+
+    const titleTags = _.map(
+      _.filter(seoTags, { tagName: 'title' }),
+      'content'
+    );
+
+    return `${titleTags.join(', ')}`;
+  }
+
+  renderContent = (data) => {
+    const article = this.props.data.markdownRemark.frontmatter;
+
+    let content = [];
+
+    if (article.images) {
+      _.map(article.images, (image) => {
+        content.push(
+          <img
+            className={style.image}
+            src={image.image.url}
+            alt={image.image.title}
+          />
+        );
+      });
+    }
+
+    if (article.video) {
+      content.push(
+        <ReactPlayer
+          className={style.video}
+          url={article.video.url}
+          width="100%"
+          height="auto"
+        />
+      );
+    }
+
+    if (article.audio) {
+      content.push(
+        <ReactAudioPlayer
+          className={style.audio}
+          src={article.audio}
+          controls
+        />
+      );
+    }
+
+    return (
+      <div className={style.content}>
+        {
+          _.map(content, (item) => {
+            return (
+              <div>
+                {item}
+              </div>
+            );
+          })
+        }
+      </div>
     );
   }
 
-  if (data.audio) {
-    content.push(
-      <ReactAudioPlayer
-        className={style.audio}
-        src={data.audio}
-        controls
-      />
-    );
-  }
+  render() {
+    const article = this.props.data.markdownRemark.frontmatter;
 
-  return (
-    <div className={style.content}>
-      {
-        _.map(content, (item) => {
-          return (
-            <div>
-              {item}
+    const paragraphClasses = cn({
+      [style.largeText]: this.state.hasOneContentItem &&
+        this.state.textLength <= 200
+    });
+
+    return (
+      <div>
+        <Helmet
+          title={`${article.title} | ${config.siteMetadata.title}`}
+        />
+
+        <div className={style.container}>
+          {this.renderContent()}
+
+          <div className={style.text}>
+            <div className={style.issue}>
+              { /* Note to self: For now, hardcoding this: */ }
+              Issue 1: Sex Ed
             </div>
-          );
-        })
-      }
-    </div>
-  );
-}
-
-const Article = ({ data }) => {
-  const article = data.markdownRemark.frontmatter;
-
-  return (
-    <div>
-      <Helmet
-        title={`${article.title} | ${config.siteMetadata.title}`}
-      />
-
-      <div className={style.container}>
-        {renderContent(article)}
-
-        <div className={style.text}>
-          <div className={style.issue}>
-            { /* Note to self: For now, hardcoding this: */ }
-            Issue 1: Sex Ed
+            <h1 className={style.title}>{article.title}</h1>
+            <h2>{article.author.name}</h2>
+            <p className={paragraphClasses}>
+              {md.render(article.text)}
+            </p>
+            <p>
+              <strong>Tags: </strong>
+              {this.getTags()}
+            </p>
           </div>
-          <h1 className={style.title}>{article.title}</h1>
-          <h2>{article.author.name}</h2>
-          <p>
-            {md.render(article.text)}
-          </p>
-          <p>
-            <strong>Tags: </strong>
-            {getTags(article.tags)}
-          </p>
         </div>
       </div>
-    </div>
-  )
-};
+    );
+  }
+}
 
 export default Article;
 
